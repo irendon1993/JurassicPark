9f7cdefafd29272e912a046f3b14a874147623b5
Isaac Rendon <isaacrendon7@gmail.com>
2020-08-19 13:59:01 -0400

    Getting there

--- Program.cs
+++ Program.cs
@@ -2,11 +2,58 @@
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    //  PROBLEM) Create a Park for dinosours  
+    //      
+    //ALGORITHM 
+    // 2. Create dinosours()
+    class Dinosour
     {
-      Console.WriteLine("Welcome to C#");
+
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public DateTime TimeAcquired { get; set}
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; };
+
+        // Description() isnt wokring yet
+        //   {
+        //   // / Dinosour will have:  
+        //   // Name = Littlefoot
+        //   // DietType = vegetarian 
+        //   // WhenAccquired = time()
+        //   // Weight = 230 lbs
+        //   // EnclosureNumber = 001
+        //    }
     }
-  }
-}
+    class Program
+    {
+
+        //
+        // 
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Jurassic Park");
+            // 3. Add dinosours to a 
+            var = new list<Dinosour>
+            {
+
+            }
+    
+      // 4. Give User options 
+      // When Console Runs, let user chose between:
+      //    View
+      //     Add
+      //     Remove
+      //     Transfer
+      //     Summary
+      //     Quit
+
+
+        }
+
+
+    }
+
